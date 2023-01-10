ce04f9fe8ef7dc3c15a1c96fe2847b1eef9a7631
SabaShagidze <109232115+SabaShagidze@users.noreply.github.com>
2023-01-10 23:30:54 +0400

    added

--- js/script.js
+++ js/script.js
@@ -17,13 +17,49 @@ document.querySelectorAll(".nav-link").forEach((n) =>
 
 // Modals
 const modalOpenButton = document.getElementById("modalOpenOne");
+const modalOpenButtonTwo = document.getElementById("modalOpenTwo");
+const modalOpenButtonThree = document.getElementById("modalOpenThree");
+const modalOpenButtonFour = document.getElementById("modalOpenFour");
+
+
 const modalOne = document.getElementById("openModalOne");
+const modalTwo = document.getElementById("openModalTwo");
+const modalThree = document.getElementById("openModalThree");
+const modalFour = document.getElementById("openModalFour");
+
+
 const modalCloseButton = document.getElementById("modalX");
-let iframe = document.querySelectorAll("iframe");
+const modalCloseButtonTwo = document.getElementById("modalXtwo");
+const modalCloseButtonThree = document.getElementById("modalXthree");
+const modalCloseButtonFour = document.getElementById("modalXfour");
+
+
 modalOpenButton.onclick = function () {
   modalOne.style.display = "flex";
+};
+modalOpenButtonTwo.onclick = function () {
+  modalTwo.style.display = "flex";
+};
+modalOpenButtonThree.onclick = function () {
+  modalThree.style.display = "flex";
+};
+modalOpenButtonFour.onclick = function () {
+  modalFour.style.display = "flex";
 };
 
 modalCloseButton.onclick = function () {
   modalOne.style.display = "none";
+  document.querySelector("video").pause();
+};
+modalCloseButtonTwo.onclick = function () {
+  modalTwo.style.display = "none";
+  document.getElementById("videoTwo").pause();
+};
+modalCloseButtonThree.onclick = function () {
+  modalThree.style.display = "none";
+  document.getElementById("videoThree").pause();
+};
+modalCloseButtonFour.onclick = function () {
+  modalFour.style.display = "none";
+  document.getElementById("videoFour").pause();
 };
