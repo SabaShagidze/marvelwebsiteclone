977d32467ad6343ac69d436e9a6c648feabca332
SabaShagidze <109232115+SabaShagidze@users.noreply.github.com>
2023-01-15 03:02:20 +0400

    added carousel

--- js/script.js
+++ js/script.js
@@ -21,18 +21,15 @@ const modalOpenButtonTwo = document.getElementById("modalOpenTwo");
 const modalOpenButtonThree = document.getElementById("modalOpenThree");
 const modalOpenButtonFour = document.getElementById("modalOpenFour");
 
-
 const modalOne = document.getElementById("openModalOne");
 const modalTwo = document.getElementById("openModalTwo");
 const modalThree = document.getElementById("openModalThree");
 const modalFour = document.getElementById("openModalFour");
 
-
 const modalCloseButton = document.getElementById("modalX");
 const modalCloseButtonTwo = document.getElementById("modalXtwo");
 const modalCloseButtonThree = document.getElementById("modalXthree");
 const modalCloseButtonFour = document.getElementById("modalXfour");
-
 
 modalOpenButton.onclick = function () {
   modalOne.style.display = "flex";
@@ -65,3 +62,29 @@ modalCloseButtonFour.onclick = function () {
 };
 
 // Swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 6,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    920: {
+      slidesPerView: 6,
+      spaceBetween: 30,
+    },
+    420: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    300: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    100: {
+      slidesPerView: 1,
+    },
+  },
+});
