bd8f9d2761ab8de1cc9186025308bec9f0b8b426
SabaShagidze <109232115+SabaShagidze@users.noreply.github.com>
2023-01-19 18:06:11 +0400

    1.19.2923

---
+++ js/character.js
@@ -0,0 +1,34 @@
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 6,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    920: {
+      slidesPerView: 6,
+      spaceBetween: 30,
+    },
+    420: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    300: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    100: {
+      slidesPerView: 1,
+    },
+  },
+});
+
+// Full Swiper
+var swiper = new Swiper(".mySwiperFull", {});
+
+useEffect(() => {
+  swiper.init();
+}, []);
