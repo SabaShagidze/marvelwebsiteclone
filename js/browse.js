4679b33518e59e54132eb00e4cf8f776a10a21e3
SabaShagidze <109232115+SabaShagidze@users.noreply.github.com>
2023-01-25 17:08:34 +0400

    1.25.2023

--- js/browse.js
+++ js/browse.js
@@ -13,3 +13,65 @@ browseBtnOne.onclick = function () {
   browseOne.style.display = "flex";
   browseTwo.style.display = "none";
 };
+
+const whoBox = {
+  whoOne: document.getElementById("whoOne"),
+  whoTwo: document.getElementById("whoTwo"),
+  whoThree: document.getElementById("whoThree"),
+  whoFour: document.getElementById("whoFour"),
+  whoFive: document.getElementById("whoFive"),
+  whoSix: document.getElementById("whoSix"),
+};
+
+const whoInnerBox = {
+  whoInnerOne: document.getElementById("whoInnerOne"),
+  whoInnerTwo: document.getElementById("whoInnerTwo"),
+  whoInnerThree: document.getElementById("whoInnerThree"),
+  whoInnerFour: document.getElementById("whoInnerFour"),
+  whoInnerFive: document.getElementById("whoInnerFive"),
+  whoInnerSix: document.getElementById("whoInnerSix"),
+};
+
+whoBox.whoOne.onmouseenter = function () {
+  whoInnerBox.whoInnerOne.style.display = "flex";
+};
+whoBox.whoOne.onmouseleave = function () {
+  whoInnerBox.whoInnerOne.style.display = "none";
+};
+
+whoBox.whoTwo.onmouseenter = function () {
+  whoInnerBox.whoInnerTwo.style.display = "flex";
+};
+whoBox.whoTwo.onmouseleave = function () {
+  whoInnerBox.whoInnerTwo.style.display = "none";
+};
+
+whoBox.whoThree.onmouseenter = function () {
+  whoInnerBox.whoInnerThree.style.display = "flex";
+};
+whoBox.whoThree.onmouseleave = function () {
+  whoInnerBox.whoInnerThree.style.display = "none";
+};
+
+whoBox.whoFour.onmouseenter = function () {
+  whoInnerBox.whoInnerFour.style.display = "flex";
+};
+whoBox.whoFour.onmouseleave = function () {
+  whoInnerBox.whoInnerFour.style.display = "none";
+};
+
+whoBox.whoFive.onmouseenter = function () {
+  whoInnerBox.whoInnerFive.style.display = "flex";
+};
+whoBox.whoFive.onmouseleave = function () {
+  whoInnerBox.whoInnerFive.style.display = "none";
+};
+
+whoBox.whoSix.onmouseenter = function () {
+  whoInnerBox.whoInnerSix.style.display = "flex";
+};
+whoBox.whoSix.onmouseleave = function () {
+  whoInnerBox.whoInnerSix.style.display = "none";
+};
+
+
--- js/character.js
+++ js/character.js
@@ -33,4 +33,3 @@ useEffect(() => {
   swiper.init();
 }, []);
 
-// browse
